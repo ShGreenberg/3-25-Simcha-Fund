b1f585bb7a5e3bc2bc56663b2d0b37697480e5bc
Shira Greenberg <greenbergshira18@gmail.com>
2019-03-26 17:48:15 -0400

    Add project files.

---
+++ 3-25 simcha-fund/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _3_25_simcha_fund
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 3-25 simcha-fund/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3_25_simcha_fund.MvcApplication" Language="C#" %>
